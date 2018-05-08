91f4cb6c4371a1f8e3f9e64b2007887739e78da6
davsip <david.sipala@gmail.com>
2018-05-08 15:42:16 -0500

    changes

--- db/schema.sql
+++ db/schema.sql
@@ -1,12 +1,16 @@
 CREATE DATABASE burgers_db;
 USE burgers_db;
 
--- Create the table tasks.
+-- Create the table tasks
+
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
 CREATE TABLE burgers
 (
-id int NOT NULL AUTO_INCREMENT,
-buger_name varchar(255) NOT NULL,
-devoured TINYINT NOT NULL default 0;
-PRIMARY KEY (id)
+	id INT NOT NULL AUTO_INCREMENT,
+	burger_name VARCHAR(200) NOT NULL,
+	devoured BOOLEAN default false,
+	PRIMARY KEY(id)
 );
-
